9920cca354316f0fdf9285649340a553eff1e0ee
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-09-20 18:11:02 +0600

    global css file modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,13 +23,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  `}
       >
 
         <Navbar></Navbar>
+        
 
-        {children}
+          {children}
 
+  
         <Footer></Footer>
       </body>
     </html>
