6757d594e29a71b94f6a35ec67018a948b7e6a28
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-09-21 12:37:29 +0600

    update code

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,5 @@
 
 import HomeContainer from "@/components/home/HomeContainer";
-<<<<<<< HEAD
-import Image from "next/image";
-import Logo from "@/components/shared/Logo";
-=======
->>>>>>> 76f0b303e9b5b1bdc82de383e46939ba85d226ad
 
 export default function Home() {
   return (
