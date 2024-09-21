a0d239eb6c27f1292a622cfc5ba19c91aaf9b3e7
Robin <sultanazakia711@gmail.com>
2024-09-21 21:14:20 +0600

    timing section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Space Grotesk"', 'sans-serif'], // Adding Space Grotesk to the sans family
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      keyframes: {
-        'top-down': {
-          '0%': { transform: 'translateY(-10px)' },
-          '50%': { transform: 'translateY(10px)' },
-          '100%': { transform: 'translateY(-10px)' },
-        },
-      },
-      animation: {
-        'top-down': 'top-down 3s infinite ease-in-out',
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: ['Space Grotesk"', 'sans-serif']
+  		},
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		keyframes: {
+  			'top-down': {
+  				'0%': {
+  					transform: 'translateY(-10px)'
+  				},
+  				'50%': {
+  					transform: 'translateY(10px)'
+  				},
+  				'100%': {
+  					transform: 'translateY(-10px)'
+  				}
+  			}
+  		},
+  		animation: {
+  			'top-down': 'top-down 3s infinite ease-in-out'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'), require("tailwindcss-animate")],
 };
