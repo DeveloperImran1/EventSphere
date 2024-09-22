85ae98aad1a8dbae13912825a96394ce5da62cf3
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-09-22 10:38:31 +0600

    global css file some animation remove

---
+++ src/app/dashboard/(admin)/admin-container/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            admin dashboard er all layout and design akhane thakbe.
+        </div>
+    );
+};
+
+export default page;
