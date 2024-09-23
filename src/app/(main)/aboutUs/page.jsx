2189aecc1b996221eb326c5c04730e8ea290c38e
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-09-23 12:04:01 +0600

    Top About Section Added (About Page)

--- src/app/(main)/aboutUs/page.jsx
+++ src/app/(main)/aboutUs/page.jsx
@@ -1,10 +1,12 @@
 import MainHeader from '@/components/About/MainHeader';
+import TopAbout from '@/components/About/TopAbout';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
             <MainHeader/>
+            <TopAbout/>
         </div>
     );
 };
