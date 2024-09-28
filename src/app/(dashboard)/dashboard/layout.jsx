414b1abdb2532b97a47449ad7db2f8b3a6ace804
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-09-28 15:26:32 +0600

    sidebar modify and request for orgainser route added

--- src/app/(dashboard)/dashboard/layout.jsx
+++ src/app/(dashboard)/dashboard/layout.jsx
@@ -12,31 +12,31 @@ const DashboarLayout = ({ children }) => {
             <div className="min-h-screen bg-gray-100">
                 {/* Navbar */}
                 <div className='fixed  h-full w-full z-50'>
-                    <div className=' relative  bg-gray-200'>
-                        <div className='flex justify-start items-start md:block'>
+                    <div className=' relative  bg-gray-200 z-50'>
+                        <div className='flex justify-start items-start md:block z-50'>
                             <DashboardNavbar />
                         </div>
-                        <BsLayoutSidebar onClick={()=> setShowSidebar(!showSidebar)} size={23} className='absolute right-5 top-[40%] text-black md:hidden cursor-pointer'></BsLayoutSidebar>
+                        <BsLayoutSidebar onClick={() => setShowSidebar(!showSidebar)} size={23} className='absolute right-5 top-[40%] text-black md:hidden cursor-pointer'></BsLayoutSidebar>
                     </div>
                 </div>
 
                 <div className="flex flex-col md:flex-row gap-4 p-4 pt-[90px] ">
                     {/* Sidebar */}
-                    {/* <div className={`${showSidebar ? 'block': 'hidden'}  md:flex fixed h-full w-full z-50`}> */}
-                    <div className={`${showSidebar ? 'left-0 top-[77px] md:top-[90px]': '-left-[500px] md:left-0 '}  absolute  h-full w-full z-50 duration-300 `}>
-                        <div className='fixed'>
-                        <DashboardSideBar />
+                    <div className={` md:flex  h-full `}>
+                        <div className={`${showSidebar ? 'left-0 top-[77px] md:top-[90px]' : '-left-[500px] md:left-0 '}  absolute  h-full z-50 duration-300 `}>
+                            <div className='fixed'>
+                                <DashboardSideBar />
+                            </div>
+                        </div>
+
+                        <div className='md:pl-[270px] overflow-auto'>
+                            {
+                                children
+                            }
                         </div>
                     </div>
 
-                    <div className='md:pl-[270px] '>
-                        {
-                            children
-                        }
-                    </div>
                 </div>
-
-     
             </div>
 
         </>
