7bd1f6ba2f26f21f28bd6b90c44204d4798fda05
Asad Atik <asadatik1995@gmail.com>
2024-09-30 00:24:00 +0600

    Created Payment Getway Page

---
+++ src/app/payment-getway/page.js
@@ -0,0 +1,12 @@
+import EnhancedPaymentGateway from '@/components/PayMentGetway/Paymentgetway';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <EnhancedPaymentGateway></EnhancedPaymentGateway>
+        </div>
+    );
+};
+
+export default page;
