7d9dc8f3a6122e41a135a08c19046c1d43fa4f1c
Tauhid hossen <tauhidhossen04@gmail.com>
2024-10-01 19:05:27 +0600

    Modifi button

--- src/components/dashboard/users/UserDashBoardHome/Social.jsx
+++ src/components/dashboard/users/UserDashBoardHome/Social.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FaLink } from "react-icons/fa6";
 import { FaCamera } from "react-icons/fa";
+import { MdLocalPostOffice } from "react-icons/md";
 const Social = () => {
     return (
         <div className='flex justify-start pl-4 items-center gap-6'>
             <div className='p-2 bg-green-200 rounded-md'><FaLink /></div>
             <div className='p-2 bg-green-200 rounded-md'><FaCamera /></div>
-            <div><button className='px-6 py-3 bg-green-300 rounded-md text-gray-600 font-bold text-xl'>Post</button></div>
+            <div><button className="w-full gap-2 sm:w-auto ml-4 flex items-center  text-xl px-3 py-2 rounded-md bg-green-400 font-bold ">
+            <MdLocalPostOffice /> Post</button>
+            </div>
         </div>
     );
 };
