9f1eac050726fcd5c8e736bc20c7394efd6c92d7
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-10-18 09:04:45 +0600

    ticket design and functionality

--- src/components/qrCode/QrCodeGen.jsx
+++ src/components/qrCode/QrCodeGen.jsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSearchParams } from 'next/navigation';
 import QRCode from 'react-qr-code';
+import Loading from '../shared/LoadingSpiner/Loading';
+import Image from 'next/image';
 
 const PaymentQRCodePage = () => {
   const searchParams = useSearchParams();
   const transitionId = searchParams.get("transitionId");
   const [paymentData, setPaymentData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (!transitionId) {
@@ -29,9 +31,10 @@ const PaymentQRCodePage = () => {
 
     fetchPaymentData();
   }, [transitionId]);
+  console.log(paymentData)
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <Loading />;
   }
 
   if (!paymentData) {
@@ -39,13 +42,53 @@ const PaymentQRCodePage = () => {
   }
 
   return (
-    <div className="payment-info">
-      <h2>Payment Successful</h2>
-      <p>Event: {paymentData.eventName}</p>
-      <p>Booked by: {paymentData.bookedUserName}</p>
-      <p>Amount Paid: ${paymentData.amount}</p>
-      <p>Transaction ID: {paymentData.transitionId}</p>
-      <QRCode value={paymentData.transitionId} />
+    <div className="payment-info flex w-11/12 mx-auto my-16 shadow-2xl justify-between p-10 bg-[#E3EAFF] rounded-2xl">
+      <div className='w-96'>
+        {/* <h2>Payment Successful</h2> */}
+
+        <Image
+          src={paymentData.eventImage}
+          alt='event-Image'
+          height={150}
+          width={370}
+          className='h-[150px] w-[300px] rounded-lg mb-5 rounded-tl-full rounded-br-full' />
+
+        <p className='text-3xl  font-bold ml-5 mb-2 font-mono'>{paymentData.eventName}</p>
+        <p className='ml-5'>Booked by: {paymentData.bookedUserName}</p>
+      </div>
+
+      <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-5xl font-bold text-blue-500 mb-10 font-mono'>Event Tickets</h1>
+        <table className='border-2 border-blue-800 border-collapse mb-5 bg-white font-mono' >
+          <tr className='border-2 p-3'>
+            <td className='border-2 p-2 border-blue-500 text-center'>Total Seats</td>
+            <td className='border-2 p-2 border-blue-500 text-center'>{paymentData.totalTickets}</td>
+          </tr>
+          <tr>
+            <td className='border-2 p-2 border-blue-500 text-center'>Seat Number</td>
+            <td className='border-2 p-2 border-blue-500 text-center'>
+              <ol className='flex gap-3 flex-wrap'>
+                {paymentData?.selectSeatNames?.map((seatName, index) => (
+                  <li key={index} className='list-none flex'>{seatName}</li>
+                ))}
+              </ol>
+            </td>
+
+          </tr>
+          <tr>
+            <td className='border-2 p-2 border-blue-500 text-center'>Total Paid</td>
+            <td className='border-2 p-2 border-blue-500 text-center'>{paymentData.amount}$</td>
+          </tr>
+        </table>
+        <div className="mb-2">
+          <span className="font-bold">Date: </span>
+          {paymentData.eventDate?.slice(0, 10) + " " + "Time:" + paymentData?.eventDate?.slice(11, 16)} AM
+        </div>
+      </div>
+      <div>
+        <QRCode value={paymentData.transitionId} className='mb-3 text-center m-2 mx-auto'/>
+        <p>Transaction ID: {paymentData.transitionId}</p>
+      </div>
     </div>
   );
 };
