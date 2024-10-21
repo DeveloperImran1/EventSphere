c590db6fdfb4d96bdb5611693ee384ccf35c582e
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-10-21 20:39:15 +0600

    chatting app related code added

--- src/components/Messenger/ChatList.jsx
+++ src/components/Messenger/ChatList.jsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { IoIosSearch } from "react-icons/io";
 
-function ChatList() {
-
+function ChatList({ conversations, currentUser }) {
+    console.log(conversations, currentUser)
     const profileNumsArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     return (
         <>
             <section className="p-5">
                 <div className="flex justify-between items-center ">
                     <div className="flex justify-between items-center gap-2">
-                        <Image height={676} width={1200}  src="https://scontent.fdac157-1.fna.fbcdn.net/v/t39.30808-1/409190995_2451672608340062_5052422953989411988_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=hh-ke09yg-gQ7kNvgERag7_&_nc_ht=scontent.fdac157-1.fna&oh=00_AYBrh0gfCKq1rErk1yb5DiQ3fzOpgeX1Fh8FuEEFVeb12A&oe=66D37960" alt="" className="w-10 h-auto rounded-full" />
+                        <Image height={676} width={1200} src="https://scontent.fdac157-1.fna.fbcdn.net/v/t39.30808-1/409190995_2451672608340062_5052422953989411988_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=hh-ke09yg-gQ7kNvgERag7_&_nc_ht=scontent.fdac157-1.fna&oh=00_AYBrh0gfCKq1rErk1yb5DiQ3fzOpgeX1Fh8FuEEFVeb12A&oe=66D37960" alt="" className="w-10 h-auto rounded-full" />
 
                         <div className="flex flex-col items-start">
                             <p>Imran Hassan</p>
@@ -46,14 +46,14 @@ function ChatList() {
                     {
 
 
-                        profileNumsArr.map((user, index) => <div key={index} className="flex justify-between items-center hover:bg-[#363e4b] cursor-pointer p-4 rounded-md ">
+                        conversations?.map((conversation) => <div key={conversation?._id} className="flex justify-between items-center hover:bg-[#363e4b] cursor-pointer p-4 rounded-md ">
                             <div className="flex justify-between items-center gap-2">
                                 <span className="rounded-full border-4 border-green-500">
-                                    <Image height={676} width={1200} src="https://scontent.fdac157-1.fna.fbcdn.net/v/t39.30808-1/409190995_2451672608340062_5052422953989411988_n.jpg?stp=dst-jpg_s200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=hh-ke09yg-gQ7kNvgERag7_&_nc_ht=scontent.fdac157-1.fna&oh=00_AYBrh0gfCKq1rErk1yb5DiQ3fzOpgeX1Fh8FuEEFVeb12A&oe=66D37960" alt="" className="w-9 h-auto rounded-full" />
+                                    <Image height={676} width={1200} src= {currentUser?.image || 'https://res.cloudinary.com/dqdircc96/image/upload/v1728834438/e7nhywmrjz43tmuctpwq.png'} alt="" className="w-9 h-auto rounded-full" />
                                 </span>
 
                                 <div className="flex flex-col items-start">
-                                    <p>Tanjila Akter</p>
+                                    <p>{currentUser?.name}</p>
                                     <p className="text-xs  text-text-color">Hi, broo ..</p>
                                 </div>
                             </div>
--- src/components/dashboard/organizer/organizer-container/Card.jsx
+++ src/components/dashboard/organizer/organizer-container/Card.jsx
@@ -7,71 +7,73 @@ import { FaSackDollar } from "react-icons/fa6";
 import useAxiosPublic from '@/hooks/useAxiosPublic';
 import { useQuery } from '@tanstack/react-query';
 
-const Card = ({email,data}) => {
+const Card = ({ email, data }) => {
    const axiosPublic = useAxiosPublic();
 
 
-   const {data: events = [], isPending: loading, refetch} = useQuery({
-       queryKey: ['events'], 
-       queryFn: async() =>{
-           const res = await axiosPublic.get(`/events/getMyEvent/${email}`);
-           // console.log(res.data)
-           return res.data;
-       }
+   const { data: events = [], isPending: loading, refetch } = useQuery({
+      queryKey: ['events'],
+      queryFn: async () => {
+         const res = await axiosPublic.get(`/events/getMyEvent/${email}`);
+         console.log("Fetch kore data ", res.data)
+         console.log("Props theke data ", data)
+         return res.data;
+      }
    })
 
- 
+
    const getUpcomingEventsForNextMonth = (events) => {
       // Step 1: Get the current date
       const currentDate = new Date();
-    
+
       // Step 2: Calculate the start and end dates for the next month
       const nextMonthStart = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1); // Start of next month
       const nextMonthEnd = new Date(currentDate.getFullYear(), currentDate.getMonth() + 2, 0); // End of next month
-    
+
       // Step 3: Filter the events based on the eventDate
       const upcomingEvents = events?.filter(order => {
-        const eventDate = new Date(order?.dateTime);
-        return eventDate >= nextMonthStart && eventDate <= nextMonthEnd;
+         const eventDate = new Date(order?.dateTime);
+         return eventDate >= nextMonthStart && eventDate <= nextMonthEnd;
       });
-   
-    
+
+
       return upcomingEvents;
-    };
-const upcomingEvents=getUpcomingEventsForNextMonth(events)
+   };
+   const upcomingEvents = getUpcomingEventsForNextMonth(events)
 
 
-const totalAmount = data?.reduce((total, order) => total + order?.amount, 0);
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+   const totalAmount = data?.reduce((total, order) => total + order?.amount, 0);
+   return (
+      <div>
+         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
             {/* Card 1 */}
             <div className='bg-white shadow-lg rounded-lg p-6  flex flex-col items-start'>
                <div className='flex justify-center items-center  gap-4'>
-                <div className='p-4 bg-slate-600 rounded-md'><MdEventNote size={60} className='text-violet-100  ' /></div>
-                <div>
-                <h2 className='text-3xl font-extrabold text-start text-gray-800'>{events?.length}</h2>
-                <p className='text-gray-500  text-start'>Awesome Events</p>
-                </div>
+                  <div className='p-4 bg-slate-600 rounded-md'><MdEventNote size={60} className='text-violet-100  ' /></div>
+                  <div>
+                     <h2 className='text-3xl font-extrabold text-start text-gray-800'>{events?.length}</h2>
+                     <p className='text-gray-500  text-start'>Awesome Events</p>
+                  </div>
                </div>
             </div>
             {/* Card 2 */}
             <div className='bg-white shadow-lg rounded-lg p-6 flex flex-col items-start'>
                <div className='flex justify-center items-center  gap-4'>
-                <div className='p-4 bg-green-600 rounded-md'><SiHomeassistantcommunitystore size={60} className='text-violet-100  ' /></div>
-                <div>
-                <h2 className=' text-3xl font-extrabold  text-gray-800 text-start'>{upcomingEvents.length}</h2>
-                <p className='text-gray-500 text-start'>Counter Opened</p>
-                </div>
+                  <div className='p-4 bg-green-600 rounded-md'><SiHomeassistantcommunitystore size={60} className='text-violet-100  ' /></div>
+                  <div>
+                     <h2 className=' text-3xl font-extrabold  text-gray-800 text-start'>{upcomingEvents.length}</h2>
+                     <p className='text-gray-500 text-start'>Counter Opened</p>
+                  </div>
                </div>
             </div>
             {/* Card 3 */}
             <div className='bg-white shadow-lg rounded-lg p-6 flex flex-col items-start'>
                <div className='flex justify-center items-center  gap-4'>
-                <div className='p-4 bg-violet-500 rounded-md'><TbTicket size={60} className='text-violet-100  ' /></div>
-                <div>
-                <h2 className='text-3xl font-extrabold text-gray-800 text-start'>{data?.length}</h2>
-                <p className='text-gray-500 text-start'>Ticket Sold</p>
-                </div>
+                  <div className='p-4 bg-violet-500 rounded-md'><TbTicket size={60} className='text-violet-100  ' /></div>
+                  <div>
+                     <h2 className='text-3xl font-extrabold text-gray-800 text-start'>{data?.length}</h2>
+                     <p className='text-gray-500 text-start'>Ticket Sold</p>
+                  </div>
                </div>
             </div>
          </div>
