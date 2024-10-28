122a06765c818b7d5c0b74e28a1810e97e488ccc
Tauhid hossen <tauhidhossen04@gmail.com>
2024-10-28 17:42:11 +0600

    contact icon rounded sm aded

--- src/components/organizers/Organizers.jsx
+++ src/components/organizers/Organizers.jsx
@@ -172,7 +172,7 @@ const Organizers = () => {
               {/* Add group-hover effect */}
               <div className="flex justify-between items-center gap-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300 font-bold">
                 <p className="text-sm text-gray-600 dark:text-gray-400 mb-2 pt-2 flex gap-2 items-center">
-                  <MdPeople className="text-xl " />
+                  <MdPeople className="text-xl rounded-sm" />
                   Followers: {organizer.followers}
                 </p>
                 <div className="flex items-center gap-3 text-xl ">
