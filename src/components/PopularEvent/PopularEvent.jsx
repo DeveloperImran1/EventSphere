fcc473a8fd89ba43da02d0e80a26629ce74f85e8
Asad Atik <asadatik1995@gmail.com>
2024-10-31 17:49:44 +0600

    color change in popular section in home page

--- src/components/PopularEvent/PopularEvent.jsx
+++ src/components/PopularEvent/PopularEvent.jsx
@@ -79,7 +79,7 @@ const EnhancedCountdown = ({ date }) => {
       </div>
       <div className="w-full h-1 bg-gray-200 rounded-full overflow-hidden">
         <div
-          className="h-full bg-gradient-to-r from-[#1b85db] to-indigo-500 transition-all duration-1000 ease-linear"
+          className="h-full bg-[#1b85db] transition-all duration-1000 ease-linear"
           style={{ width: `${(seconds / 60) * 100}%` }}
         ></div>
       </div>
