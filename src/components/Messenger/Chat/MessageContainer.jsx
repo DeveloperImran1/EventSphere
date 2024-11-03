a51a9930d475746dd2447ad33896818bb2ecc78f
Robin <sultanazakia711@gmail.com>
2024-11-03 23:46:50 +0600

    order data fetched for organizer

--- src/components/Messenger/Chat/MessageContainer.jsx
+++ src/components/Messenger/Chat/MessageContainer.jsx
@@ -70,7 +70,7 @@ const MessageContainer = ({ selectedUser }) => {
   const handleMessages = (e) => {
     setSendData(e.target.value);
   };
-
+// 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
