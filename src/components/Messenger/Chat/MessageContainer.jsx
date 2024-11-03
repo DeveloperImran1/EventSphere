73c52dc50cd289ce83b32db269eccfe1321c241e
Robin <sultanazakia711@gmail.com>
2024-11-04 00:03:46 +0600

    tab section added on messenger page

--- src/components/Messenger/Chat/MessageContainer.jsx
+++ src/components/Messenger/Chat/MessageContainer.jsx
@@ -70,7 +70,7 @@ const MessageContainer = ({ selectedUser }) => {
   const handleMessages = (e) => {
     setSendData(e.target.value);
   };
-// handleSubmit added wit
+// handleSubmit added with
   const handleSubmit = async (e) => {
     e.preventDefault();
 
