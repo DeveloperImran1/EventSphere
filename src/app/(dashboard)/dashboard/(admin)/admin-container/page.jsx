07d8231b661f71088a730344098e15436e9fc4ba
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-11-03 23:59:54 +0600

    meta data removed

--- src/app/(dashboard)/dashboard/(admin)/admin-container/page.jsx
+++ src/app/(dashboard)/dashboard/(admin)/admin-container/page.jsx
@@ -1,10 +1,6 @@
 import AdminInfo from "@/components/dashboard/admin/AdminInfo";
 
-export const metadata = {
-  title: "Admin dashboard",
-  description: "admin dashboard page, Smart Event Management and Booking Platform",
-  keywords: ["admin dashboard","online", "ticket", "selling", "system", "event", "management"]
-};
+
 
 const AdminContainer = () => {
   return (
