807ce3508eb49cb829b86e27f1feea464abf40b9
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2024-11-10 13:33:38 +0600

    some problem is fixed

--- src/middleware.js
+++ src/middleware.js
@@ -12,7 +12,7 @@ export const middleware = async (request) => {
     const pathname = request.nextUrl.pathname;
     console.log("path name is ", pathname)
 
-    if (pathname.includes('api')) {   // aikhane dsashboard er pore all jekono route private ase. But jodi need hoi,, dashboard er moddhe kono route k private korbona, tahole upore condition dia return korte pari,, tahole middleware kajj korbena. aikhane api er poriborte oi route name hobe.
+    if (pathname.includes('api')) {   // aikhane dsashboard er pore all jekono route private ase. But jodi need hoi,, dashboard er moddhe kono route k private korbona, tahole upore condition dia return korte pari,, tahole middleware kajj korbena. aikhane api er poriborte oi route name hobe...
         return NextResponse.next()
     }
     if (!token) {
